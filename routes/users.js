6118377569dc0345afb030ec8e47a4ecda94ddfc
arthur <arthur>
2023-07-25 10:35:12 -0300

    versão 2

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,17 @@ router.get("/", function (req, res, next) {
   res.send("respond with a resource");
 });
 
+router.get("/cool", function (req, res, next) {
+  res.send("you are so cool");
+});
+
+router.get("/cool/beach", function (req, res, next) {
+  res.send("Adoro Praia");
+});
+
+router.get("/ifc", function (req, res, next) {
+  res.send("Bem vindo ao IFC");
+});
+
+
 module.exports = router;
